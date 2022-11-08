cfe8e28cbe146587dbcc810f0ae846e01422985a
Lokeshchinnu <jlkumar1995@gmail.com>
2022-11-08 16:19:48 +0530

    Education formArray Completed

--- src/app/components/create-students/create-students.component.ts
+++ src/app/components/create-students/create-students.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-students',
@@ -21,12 +21,32 @@ export class CreateStudentsComponent implements OnInit {
           mandal:new FormControl(),
           district:new FormControl(),
           state:new FormControl(),
-          pin:new FormControl()
+          pin:new FormControl(),
         }
-      )
+        
+      ),
+      education:new FormArray([]),
     }
 
   )
+
+  get educationFormArray(){
+    return this.studentForm.get('education') as FormArray;
+  }
+
+  add(){
+    this.educationFormArray.push(
+      new FormGroup(
+        {
+          qualification:new FormControl(),
+          year:new FormControl(),
+          percentage:new FormControl()
+
+        }
+      )
+    )
+
+  }
   constructor() { }
 
   submit(){
